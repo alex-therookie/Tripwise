654d90a26a485c35a65f586c61dca9f4f0071e31
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-03 19:00:18 -0400

    Set up initial dashboard components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/Navbar/NavBar";
+import NavBar from "./components/Navbar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
+import Dashboard from "./components/Dashboard";
 import { authenticate } from "./store/session";
 
 function App() {
@@ -42,7 +43,7 @@ function App() {
           <User />
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true}>
-          <h1>My Home Page</h1>
+          <Dashboard />
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
