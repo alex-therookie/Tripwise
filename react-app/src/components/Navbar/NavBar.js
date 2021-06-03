98cf13190c426ba3f87963ff2d5b873068522a8a
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-03 15:13:03 -0400

    Refactored navbar to add menu btn and styling

--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
+import "./NavBar.css";
 // testing
 const NavBar = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
+
   return (
-    <nav>
-      <ul>
+    <nav className="nav-container">
+      <div className="nav-logo">
+        <NavLink to="/" exact={true} activeClassName="active">
+          <h2 className="nav-logo">Home</h2>
+        </NavLink>
+      </div>
+      <div
+        className="nav-menu-icon"
+        onClick={() => setMenuClicked(!menuClicked)}
+      >
+        <i className={menuClicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={menuClicked ? "nav-menu-items active" : "nav-menu-items"}>
         <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
+          <NavLink
+            to="/login"
+            exact={true}
+            activeClassName="active"
+            className="nav-menu-item"
+          >
             Login
           </NavLink>
         </li>
         <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            activeClassName="active"
+            className="nav-menu-item"
+          >
             Sign Up
           </NavLink>
         </li>
         <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
+          <NavLink
+            to="/users"
+            exact={true}
+            activeClassName="active"
+            className="nav-menu-item"
+          >
             Users
           </NavLink>
         </li>
