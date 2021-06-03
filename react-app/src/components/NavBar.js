d9a7c3dd925a8f5d0476e1d39718423aea79bb16
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-03 12:39:11 -0400

    testing comment

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
+// testing
 const NavBar = () => {
   return (
     <nav>
@@ -32,6 +32,6 @@ const NavBar = () => {
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
