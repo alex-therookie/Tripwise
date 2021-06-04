ac79f85c482a8d7d94e40050a2fb96b38535e1aa
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-04 13:30:51 -0400

    Added activity and expense models

---
+++ app/models/activity.py
@@ -0,0 +1,26 @@
+from .db import db
+from datetime import datetime
+
+class Activity(db.Model):
+    __tablename__ = 'activities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    photoUrl = db.Column(db.String(500), nullable=True)
+    date = db.Column(db.Date, nullable=True)
+    tripId = db.Column(db.Integer, nullable=False)
+    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "photoUrl": self.photoUrl,
+            "date": self.date,
+            "tripId": self.tripId,
+            "createdAt": self.createdAt
+        }
---
+++ app/models/expense.py
@@ -0,0 +1,25 @@
+from .db import db
+from datetime import datetime
+
+class Expense(db.Model):
+    __tablename__ = 'expenses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(30), nullable=False)
+    amount = db.Column(db.Numeric(10,2), nullable=False)
+    photoUrl = db.Column(db.String(500), nullable=True)
+    tripId = db.Column(db.Integer, nullable=False)
+    activityId = db.Column(db.Integer, nullable=True)
+    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "amount": self.amount,
+            "photoUrl": self.photoUrl,
+            "tripId": self.tripId,
+            "activityId": self.activityId,
+            "createdAt": self.createdAt
+        }
