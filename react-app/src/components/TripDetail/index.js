fdb1b76d1fd718f962ff95d1fbe500944c6c02e3
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-08 17:05:56 -0400

    Added styling for trips list and trip detail

--- react-app/src/components/TripDetail/index.js
+++ react-app/src/components/TripDetail/index.js
@@ -1,12 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getTrip } from "../../store/trip";
+import "./TripDetail.css";
 
 const TripDetail = () => {
+  const dispatch = useDispatch();
   const { tripId } = useParams();
-  console.log(tripId);
+  const trip = useSelector((state) => state.trip[tripId]);
 
-  return <div>Hello from Trip {tripId} Detail!</div>;
+  useEffect(() => {
+    dispatch(getTrip(tripId));
+  }, [dispatch]);
+
+  if (!trip) return null;
+
+  return (
+    <div className="trip-detail-container">
+      <div className="trip-topbar">
+        <div className="img-name-wrapper">
+          <div className="trip-img">photo</div>
+          <h2 className="trip-name">{trip.name}</h2>
+        </div>
+        <div className="trip-btn-wrapper">
+          <button className="btn btn-large btn-green">Add an expense</button>
+          <button className="btn btn-large btn-orange">Settle up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TripDetail;
--- react-app/src/components/TripSummary/index.js
+++ react-app/src/components/TripSummary/index.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { NavLink, useHistory } from "react-router-dom";
-import { getTrip } from "../../store/trip";
+import React from "react";
+import {} from "react-redux";
+import { useHistory } from "react-router-dom";
 import "./TripSummary.css";
 
 const TripSummary = ({ trip }) => {
