b47c2b5248cf2234ff72441c3888bb99a1cabbb4
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-08 18:09:16 -0400

    Added styling for activity component

--- react-app/src/components/Activity/index.js
+++ react-app/src/components/Activity/index.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
+import "./Activity.css";
 
 const Activity = ({ activity }) => {
-  return <div>This is activity! </div>;
+  return (
+    <div className="activity-container">
+      <div className="activity-date activity-details">July 4</div>
+      <div className="activity-img activity-details">photo</div>
+      <div className="activity-name activity-details">
+        <h3>{activity.name}</h3>
+      </div>
+      <div className="activity-balance activity-details">$120.00</div>
+    </div>
+  );
 };
 
 export default Activity;
