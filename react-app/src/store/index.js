4ef3abb1a12aa76e3e5831f58006e7601c3bc819
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-08 12:43:35 -0400

    Added initial setup of trip reducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,25 +1,26 @@
-import {createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import session from "./session"
+import session from "./session";
+import trip from "./trip";
 
 const rootReducer = combineReducers({
-    session,
+  session,
+  trip,
 });
-
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
-    enhancer = applyMiddleware(thunk);
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
 } else {
-    const logger = require('redux-logger').default;
-    const composeEnhancers =
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
-    return createStore(rootReducer, preloadedState, enhancer);
+  return createStore(rootReducer, preloadedState, enhancer);
 };
 
 export default configureStore;
--- react-app/src/store/trip.js
+++ react-app/src/store/trip.js
@@ -1,4 +1,69 @@
 // Notes on store blueprint
+const LOAD_TRIP = "trip/LOAD_TRIP";
+const ADD_TRIP = "trip/ADD_TRIP";
+
+export const loadTrip = (trip) => {
+  return {
+    type: LOAD_TRIP,
+    trip,
+  };
+};
+
+export const addTrip = (trip) => {
+  return {
+    type: ADD_TRIP,
+    trip,
+  };
+};
+
+export const getTrip = (tripId) => async (dispatch) => {
+  const res = await fetch(`/api/${tripId}/`);
+  if (res.ok) {
+    const trip = await res.json();
+    console.log(trip);
+    dispatch(loadTrip(trip));
+  }
+};
+
+export const postTrip = (trip) => async (dispatch) => {
+  const res = await fetch(`/api/trips/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: {
+      trip: JSON.stringify(trip),
+    },
+  });
+  const trip = await res.json();
+  console.log(trip);
+  dispatch(addTrip(trip));
+};
+
+const initialState = {};
+
+const tripReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_TRIP: {
+      const { trip } = action;
+
+      return {
+        ...state,
+        [trip.id]: trip,
+      };
+    }
+
+    case ADD_TRIP: {
+      return {
+        ...state,
+        [trip.id]: trip,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default tripReducer;
 
 // store = {
 //     user: {}, //
