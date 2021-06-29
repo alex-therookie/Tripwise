204cf1dfd75af8b82bbe4571b51502422f9271a9
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-06-29 13:05:39 -0400

    Added comments styling

--- react-app/src/components/Comment/index.js
+++ react-app/src/components/Comment/index.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import "./Comment.css";
 
 const Comment = () => {
+  const user = useSelector((state) => state.session.user);
+  console.log(user.username);
   return (
     <div className="comments-container">
       <h4>Comments</h4>
       <div className="comment">
-        <div className="comment-user"></div>
+        <div className="comment-user">{user.username} says: </div>
+        Test Comment
       </div>
       <div className="add-comment"></div>
     </div>
